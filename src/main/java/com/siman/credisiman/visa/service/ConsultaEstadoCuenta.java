321366b8dfea1c31cd247e67f305e8c86475b20e
carlossalazar7 <carlossalazar2228@gmail.com>
2022-10-04 08:20:56 -0600

    ConsultaEstadosCuentas class add

---
+++ src/main/java/com/siman/credisiman/visa/service/ConsultaEstadoCuenta.java
@@ -0,0 +1,34 @@
+package com.siman.credisiman.visa.service;
+
+import org.apache.xmlbeans.XmlCursor;
+import org.apache.xmlbeans.XmlObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.xml.namespace.QName;
+
+public class ConsultaEstadoCuenta {
+    private static final Logger log = LoggerFactory.getLogger(ConsultaEstadoCuenta.class);
+
+    public static XmlObject obtenerConsultaEstadoCuenta(String pais, String numeroTarjeta,String cuenta,
+                                                        String fechaCorte, String remoteJndi) {
+        String namespace = "http://siman.com/ConsultaEstadoCuenta";
+        String operationResponse = "ObtenerConsultaEstadoCuentaResponse";
+        //OBTENER DATOS
+        XmlObject result = XmlObject.Factory.newInstance();
+        XmlCursor cursor = result.newCursor();
+        QName responseQName = new QName(namespace, operationResponse);
+        cursor.toNextToken();
+        cursor.beginElement(responseQName);
+        cursor.insertElementWithText(new QName(namespace, "statusCode"), "00");
+        cursor.insertElementWithText(new QName(namespace, "status"), "SUCCESS");
+        cursor.insertElementWithText(new QName(namespace, "statusMessage"), "Proceso exitoso");
+        cursor.insertElementWithText(new QName(namespace, "correo"),"juan.perez@correo.com");
+        cursor.insertElementWithText(new QName(namespace, "archivo"),"StringBase24");
+        cursor.toParent();
+
+        log.info("obtenerConsultaEstadoCuenta response = [" + result.toString() + "]");
+        return result;
+    }
+
+}
---
+++ src/test/java/com/siman/credisiman/visa/service/ConsultaEstadoCuentaTest.java
@@ -0,0 +1,24 @@
+package com.siman.credisiman.visa.service;
+
+import org.apache.xmlbeans.SimpleValue;
+import org.apache.xmlbeans.XmlObject;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConsultaEstadoCuentaTest {
+    static private String NS = "http://siman.com/ConsultaEstadoCuenta";
+
+    @Test
+    public void obtenerDatosClienteOk() {
+        XmlObject result = ConsultaEstadoCuenta.obtenerConsultaEstadoCuenta("SLV", "4000123456780000", "12345","20220718","jndi/ArcaSV");
+        int i = 0;
+
+        //Status
+        assertEquals("00", ((SimpleValue) result.selectPath("declare namespace ns='" + NS + "' " + ".//ns:statusCode")[0]).getStringValue());
+        assertEquals("SUCCESS", ((SimpleValue) result.selectPath("declare namespace ns='" + NS + "' " + ".//ns:status")[0]).getStringValue());
+        assertEquals("juan.perez@correo.com", ((SimpleValue) result.selectPath("declare namespace ns='" + NS + "' " + ".//ns:correo")[0]).getStringValue());
+        assertEquals("StringBase24", ((SimpleValue) result.selectPath("declare namespace ns='" + NS + "' " + ".//ns:archivo")[0]).getStringValue());
+
+    }
+}

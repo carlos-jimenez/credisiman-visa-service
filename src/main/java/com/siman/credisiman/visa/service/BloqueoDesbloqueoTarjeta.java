605c2c11d859b6b0b3ba1061119c89820e87ffc6
carlossalazar7 <carlossalazar2228@gmail.com>
2022-10-04 09:01:04 -0600

    BloqueoDesbloqueoTarjeta class add

---
+++ src/main/java/com/siman/credisiman/visa/service/BloqueoDesbloqueoTarjeta.java
@@ -0,0 +1,30 @@
+package com.siman.credisiman.visa.service;
+
+import org.apache.xmlbeans.XmlCursor;
+import org.apache.xmlbeans.XmlObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.xml.namespace.QName;
+
+public class BloqueoDesbloqueoTarjeta {
+    private static final Logger log = LoggerFactory.getLogger(BloqueoDesbloqueoTarjeta.class);
+
+    public static XmlObject obtenerBloqueoDesbloqueoTarjeta(String pais, String numeroTarjeta,String estadoDeseado, String motivo, String remoteJndi) {
+        String namespace = "http://siman.com/BloqueoDesbloqueoTarjeta";
+        String operationResponse = "ObtenerBloqueoDesbloqueoTarjetaResponse";
+        //OBTENER DATOS
+
+        XmlObject result = XmlObject.Factory.newInstance();
+        XmlCursor cursor = result.newCursor();
+        QName responseQName = new QName(namespace, operationResponse);
+        cursor.toNextToken();
+        cursor.beginElement(responseQName);
+        cursor.insertElementWithText(new QName(namespace, "statusCode"), "00");
+        cursor.insertElementWithText(new QName(namespace, "status"), "SUCCESS");
+        cursor.insertElementWithText(new QName(namespace, "statusMessage"), "Proceso exitoso");
+        cursor.toParent();
+        log.info("obtenerBloqueoDesbloqueoTarjeta response = [" + result.toString() + "]");
+        return result;
+    }
+}
---
+++ src/test/java/com/siman/credisiman/visa/service/BloqueoDesbloqueoTarjetaTest.java
@@ -0,0 +1,21 @@
+package com.siman.credisiman.visa.service;
+
+import org.apache.xmlbeans.SimpleValue;
+import org.apache.xmlbeans.XmlObject;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BloqueoDesbloqueoTarjetaTest {
+    static private String NS = "http://siman.com/BloqueoDesbloqueoTarjeta";
+
+    @Test
+    public void obtenerConsultaSubProductosOk() {
+        XmlObject result = BloqueoDesbloqueoTarjeta.obtenerBloqueoDesbloqueoTarjeta("SLV", "4000123456780000","01","Robo", "jndi/ArcaSV");
+
+        //Status
+        assertEquals("00", ((SimpleValue) result.selectPath( "declare namespace ns='" + NS + "' " + ".//ns:statusCode")[0]).getStringValue());
+        assertEquals("SUCCESS", ((SimpleValue) result.selectPath( "declare namespace ns='" + NS + "' " + ".//ns:status")[0]).getStringValue());
+
+    }
+}

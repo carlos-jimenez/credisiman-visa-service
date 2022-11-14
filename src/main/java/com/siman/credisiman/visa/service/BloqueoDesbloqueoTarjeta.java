1ba93a33971e3771da682fd261c88d48c711c1b7
carlossalazar7 <carlossalazar2228@gmail.com>
2022-11-14 12:01:57 -0600

    servicio ConsultaMovimientos, agregar query

--- src/main/java/com/siman/credisiman/visa/service/BloqueoDesbloqueoTarjeta.java
+++ src/main/java/com/siman/credisiman/visa/service/BloqueoDesbloqueoTarjeta.java
@@ -17,57 +17,57 @@ import javax.xml.namespace.QName;
 public class BloqueoDesbloqueoTarjeta {
     private static final Logger log = LoggerFactory.getLogger(BloqueoDesbloqueoTarjeta.class);
 
-	public static XmlObject obtenerBloqueoDesbloqueoTarjeta(String pais, String numeroTarjeta, String estadoDeseado,
-			String motivo, String remoteJndiSunnel, String remoteJndiOrion, String siscardUrl, String siscardUser,
-			String binCredisiman, String tipoTarjeta) {
+    public static XmlObject obtenerBloqueoDesbloqueoTarjeta(String pais, String numeroTarjeta, String estadoDeseado,
+                                                            String motivo, String remoteJndiSunnel, String remoteJndiOrion, String siscardUrl, String siscardUser,
+                                                            String binCredisiman, String tipoTarjeta) {
         String namespace = "http://siman.com/BloqueoDesbloqueoTarjeta";
         String operationResponse = "ObtenerBloqueoDesbloqueoTarjetaResponse";
-        
+
         BloqueoDesbloqueoTarjetaResponse response1 = new BloqueoDesbloqueoTarjetaResponse();
         //OBTENER DATOS
         try {
-    		JSONObject jsonSend = new JSONObject(); //json a enviar
-    		jsonSend.put("pais", pais)
-    		.put("processIdentifier", "SolicitudReposicionTarjeta")
-    		.put("tipoMensaje", 4300)
-    		.put("numeroTarjeta", numeroTarjeta)
-    		.put("estadoDeseado", estadoDeseado)
-    		.put("motivo", motivo)
-    		.put("usuarioSiscard", siscardUser);
-    		
-    		HttpResponse<String> jsonResponse //realizar petición mediante unirest
-    		= Unirest.post(siscardUrl)
-    		.header("Content-Type", "application/json")
-    		.body(jsonSend.toString())
-    		.asString();
-    		
-    		//capturar respuesta
-    		JSONObject response = new JSONObject(jsonResponse
-    		.getBody()
-    		.replaceAll("\u200B", ""));
-    		response1 = new ObjectMapper()
-    		.readValue(response.toString(), BloqueoDesbloqueoTarjetaResponse.class);
-    		
-    		log.info(new ObjectMapper().writeValueAsString(response1));
-    		} catch (Exception e) {
-    		e.printStackTrace();
-    		log.info(e.getMessage());
-    		}
-    		
-    		
-    		XmlObject result = XmlObject.Factory.newInstance();
-    		XmlCursor cursor = result.newCursor();
-    		QName responseQName = new QName(namespace, operationResponse);
-    		
-    		cursor.toNextToken();
-    		cursor.beginElement(responseQName);
-    		
-    		cursor.insertElementWithText(new QName(namespace, "statusCode"),  response1.statusCode);
-    		cursor.insertElementWithText(new QName(namespace, "status"), response1.getStatus());
-    		cursor.insertElementWithText(new QName(namespace, "statusMessage"), response1.getStatusMessage());
+            JSONObject jsonSend = new JSONObject(); //json a enviar
+            jsonSend.put("pais", pais)
+                    .put("processIdentifier", "SolicitudReposicionTarjeta")
+                    .put("tipoMensaje", 4300)
+                    .put("numeroTarjeta", numeroTarjeta)
+                    .put("estadoDeseado", estadoDeseado)
+                    .put("motivo", motivo)
+                    .put("usuarioSiscard", siscardUser);
 
-    		cursor.toParent();
-        
+            HttpResponse<String> jsonResponse //realizar petición mediante unirest
+                    = Unirest.post(siscardUrl)
+                    .header("Content-Type", "application/json")
+                    .body(jsonSend.toString())
+                    .asString();
+
+            //capturar respuesta
+            JSONObject response = new JSONObject(jsonResponse
+                    .getBody()
+                    .replaceAll("\u200B", ""));
+            response1 = new ObjectMapper()
+                    .readValue(response.toString(), BloqueoDesbloqueoTarjetaResponse.class);
+
+            log.info(new ObjectMapper().writeValueAsString(response1));
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info(e.getMessage());
+        }
+
+
+        XmlObject result = XmlObject.Factory.newInstance();
+        XmlCursor cursor = result.newCursor();
+        QName responseQName = new QName(namespace, operationResponse);
+
+        cursor.toNextToken();
+        cursor.beginElement(responseQName);
+
+        cursor.insertElementWithText(new QName(namespace, "statusCode"), response1.statusCode);
+        cursor.insertElementWithText(new QName(namespace, "status"), response1.getStatus());
+        cursor.insertElementWithText(new QName(namespace, "statusMessage"), response1.getStatusMessage());
+
+        cursor.toParent();
+
 
         log.info("obtenerBloqueoDesbloqueoTarjeta response = [" + result.toString() + "]");
         return result;

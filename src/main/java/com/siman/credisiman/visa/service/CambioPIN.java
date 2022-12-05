2abd0767d4055bd90ccbed377a6afba8eeaf8202
carlossalazar7 <carlossalazar2228@gmail.com>
2022-12-05 12:22:40 -0600

    Servicio CambioPIN

--- src/main/java/com/siman/credisiman/visa/service/CambioPIN.java
+++ src/main/java/com/siman/credisiman/visa/service/CambioPIN.java
@@ -15,14 +15,15 @@ import org.slf4j.LoggerFactory;
 import javax.xml.namespace.QName;
 
 public class CambioPIN {
-    private static Logger log = LoggerFactory.getLogger(CambioPIN.class);
+    private static final Logger log = LoggerFactory.getLogger(CambioPIN.class);
+    private static final String namespace = "http://siman.com/CambioPIN";
+    private static final String operationResponse = "CambioPINResponse";
 
     public static XmlObject cambioPIN(String pais, String numeroTarjeta, String nip, String remoteJndiSunnel,
-                                      String remoteJndiOrion, String siscardUrl, String siscardUser, String binCredisiman) {
-        String namespace = "http://siman.com/CambioPIN";
-        String operationResponse = "CambioPINResponse";
+                                      String remoteJndiOrion, String siscardUrl, String siscardUser, String binCredisiman, String tipoTarjeta) {
+
         //OBTENER DATOS
-        CambioPinResponse response1 = new CambioPinResponse();
+
         Utils utils = new Utils();
         Message message = new Message();
 
@@ -39,56 +40,84 @@ public class CambioPIN {
         }
 
         try {
-            JSONObject jsonSend = new JSONObject(); //json a enviar
-            jsonSend.put("country", pais)
-                    .put("processIdentifier", "ConsultaTransaccionesNotificaciones")
-                    .put("numeroTarjeta", numeroTarjeta)
-                    .put("nip", nip);
-
-            HttpResponse<String> jsonResponse //realizar petición mediante unirest
-                    = Unirest.post(siscardUrl.concat("/cambioPIN"))
-                    .header("Content-Type", "application/json")
-                    .body(jsonSend.toString())
-                    .asString();
-
-            //capturar respuesta
-            JSONObject response = new JSONObject(jsonResponse
-                    .getBody());
-            response1 = new ObjectMapper()
-                    .readValue(response.toString(), CambioPinResponse.class);
-
-            log.info(new ObjectMapper().writeValueAsString(response1));
-
-            XmlObject result = XmlObject.Factory.newInstance();
-            XmlCursor cursor = result.newCursor();
-            QName responseQName = new QName(namespace, operationResponse);
-
-            cursor.toNextToken();
-            cursor.beginElement(responseQName);
-
-            String mensaje = null;
-            String estado = null;
-            String codigo = null;
-            if(response1.getRespuestas()!= null){
-                mensaje =  response1.getRespuestas().get(0).getStatusMessage();
-                estado = response1.getRespuestas().get(0).getStatus();
-                codigo = response1.getRespuestas().get(0).getStatusCode();
+            switch (tipoTarjeta) {
+                case "P":
+                    //datos tarjeta privada
+                    CambioPinResponse response1 = new CambioPinResponse();
+                    response1 = obtenerDatosEvertec(pais, numeroTarjeta, nip, siscardUrl);
+                    if (response1 != null) {
+                        return estructura(response1);
+                    }
+                    break;
+                case "V":
+                    //datos tarjeta visa
+                    CambioPinResponse response2 = obtenerDatosEvertec(pais, numeroTarjeta, nip, siscardUrl);
+                    if (response2 != null) {
+                        return estructura(response2);
+                    }
+                default: message.genericMessage("ERROR", "400", "Tipo tarjeta no valida", namespace, operationResponse);
+                    break;
             }
-            if(mensaje != null){
-                cursor.insertElementWithText(new QName(namespace, "statusCode"),codigo);
-                cursor.insertElementWithText(new QName(namespace, "status"), estado);
-                cursor.insertElementWithText(new QName(namespace, "statusMessage"),mensaje);
-            }else{
-                cursor.insertElementWithText(new QName(namespace, "statusCode"),response1.getStatusCode());
-                cursor.insertElementWithText(new QName(namespace, "status"), response1.getStatus());
-                cursor.insertElementWithText(new QName(namespace, "statusMessage"), response1.getStatusMessage());
-            }
-            cursor.toParent();
-            log.info("obtenerCambioPIN response = [" + result.toString() + "]");
-            return result;
         } catch (Exception e) {
             e.printStackTrace();
             return message.genericMessage("ERROR", "600", "Error general contacte al administrador del sistema...", namespace, operationResponse);
         }
+        log.info("CambioPIN response = [" + message.genericMessage("ERROR", "400", "La consulta no devolvio resultados", namespace, operationResponse) + "]");
+        return message.genericMessage("ERROR", "400", "No se encontro el numero de tarjeta", namespace, operationResponse);
+    }
+
+
+    public static XmlObject estructura(CambioPinResponse response1) {
+        XmlObject result = XmlObject.Factory.newInstance();
+        XmlCursor cursor = result.newCursor();
+        QName responseQName = new QName(namespace, operationResponse);
+
+        cursor.toNextToken();
+        cursor.beginElement(responseQName);
+
+        String mensaje = null, estado = null, codigo = null;
+
+        if (response1.getRespuestas() != null) {
+            mensaje = response1.getRespuestas().get(0).getStatusMessage();
+            estado = response1.getRespuestas().get(0).getStatus();
+            codigo = response1.getRespuestas().get(0).getStatusCode();
+        }
+        if (mensaje != null) {
+            cursor.insertElementWithText(new QName(namespace, "statusCode"), codigo);
+            cursor.insertElementWithText(new QName(namespace, "status"), estado);
+            cursor.insertElementWithText(new QName(namespace, "statusMessage"), mensaje);
+        } else {
+            cursor.insertElementWithText(new QName(namespace, "statusCode"), response1.getStatusCode());
+            cursor.insertElementWithText(new QName(namespace, "status"), response1.getStatus());
+            cursor.insertElementWithText(new QName(namespace, "statusMessage"), response1.getStatusMessage());
+        }
+        cursor.toParent();
+        log.info("obtenerCambioPIN response = [" + result.toString() + "]");
+        return result;
+    }
+
+    public static CambioPinResponse obtenerDatosEvertec(String pais, String numeroTarjeta, String nip, String siscardUrl) throws Exception {
+        JSONObject jsonSend = new JSONObject(); //json a enviar
+        CambioPinResponse response1 = new CambioPinResponse();
+
+        jsonSend.put("country", pais)
+                .put("processIdentifier", "ConsultaTransaccionesNotificaciones")
+                .put("numeroTarjeta", numeroTarjeta)
+                .put("nip", nip);
+
+        HttpResponse<String> jsonResponse //realizar petición mediante unirest
+                = Unirest.post(siscardUrl.concat("/cambioPIN"))
+                .header("Content-Type", "application/json")
+                .body(jsonSend.toString())
+                .asString();
+
+        //capturar respuesta
+        JSONObject response = new JSONObject(jsonResponse
+                .getBody());
+        response1 = new ObjectMapper()
+                .readValue(response.toString(), CambioPinResponse.class);
+
+        log.info(new ObjectMapper().writeValueAsString(response1));
+        return response1;
     }
 }
--- src/test/java/com/siman/credisiman/visa/service/CambioPINTest.java
+++ src/test/java/com/siman/credisiman/visa/service/CambioPINTest.java
@@ -11,7 +11,7 @@ public class CambioPINTest {
 
     @Test
     public void obtenerDatosClienteOk() {
-        XmlObject result = CambioPIN.cambioPIN("SV", "4000123456780000", "xOCcB1WjksWWqbK/o8taUQ==","jdbc/SUNTST", "jdbc/ORIONREPOSV", "http://soauat.siman.com:7003/v1/orion", "CDS00867", "600831, 600831, 600831");
+        XmlObject result = CambioPIN.cambioPIN("SV", "4000123456780000", "xOCcB1WjksWWqbK/o8taUQ==","jdbc/SUNTST", "jdbc/ORIONREPOSV", "http://soauat.siman.com:7003/v1/orion", "CDS00867", "600831, 600831, 600831","V");
 
         //Status
         assertEquals("00", ((SimpleValue) result.selectPath("declare namespace ns='" + NS + "' " + ".//ns:statusCode")[0]).getStringValue());
